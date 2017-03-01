c28ccd55193a908d6e75fb1b3582021a4591ba10
Sam1920Chanow <sam.chanow@griffinschool.org>
2017-03-01 11:51:24 -0600

    Prints cool circle in client

--- src/main/java/in/voidma/classroom/network/client/Client.java
+++ src/main/java/in/voidma/classroom/network/client/Client.java
@@ -8,12 +8,19 @@ public class Client extends PApplet {
     @Override
     public void setup() {
         super.setup();
-        Network n = new Network(this);
     }
+
+    @Override
+    public void settings() {
+        size(700, 700);
+    }
+
 
     @Override
     public void draw() {
         super.draw();
+
+        ellipse(50, 50, 50, 50);
     }
 
     public static void main(String[] args) {
