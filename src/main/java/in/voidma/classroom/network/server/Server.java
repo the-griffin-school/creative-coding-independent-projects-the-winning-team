89f3a470356101152edff683b4e811554c70991e
Silberling-Cook <miles.silberlingcook@gmail.com>
2017-03-07 11:54:42 -0600

    Started server implementation

--- src/main/java/in/voidma/classroom/network/server/Server.java
+++ src/main/java/in/voidma/classroom/network/server/Server.java
@@ -1,24 +1,44 @@
 package in.voidma.classroom.network.server;
 
+import in.voidma.classroom.network.server.network.ServerChannelInitializer;
+import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 public class Server {
 
     private int port;
 
-    private EventLoopGroup bossGroup;
-    private EventLoopGroup workerGroup;
     private ChannelFuture channel;
 
-    public Server(int port) {
+    private Server(int port) {
         this.port = port;
     }
 
     public static void main (String[] args) {
-
+        Server server = new Server(8118);
+        server.run();
     }
 
-    void run() {
+    private void run() {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(bossGroup, workerGroup);
+        bootstrap.channel(NioServerSocketChannel.class);
+
+        bootstrap.childHandler(new ServerChannelInitializer());
+        bootstrap.childOption(ChannelOption.SO_KEEPALIVE, true);
+
+        try {
+            channel = bootstrap.bind(port).sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/in/voidma/classroom/network/server/network/ServerChannelInitializer.java
+++ src/main/java/in/voidma/classroom/network/server/network/ServerChannelInitializer.java
@@ -2,11 +2,17 @@ package in.voidma.classroom.network.server.network;
 
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.util.concurrent.DefaultEventExecutorGroup;
+import io.netty.util.concurrent.EventExecutorGroup;
 
 public class ServerChannelInitializer extends ChannelInitializer {
 
+    private final EventExecutorGroup group = new DefaultEventExecutorGroup(1500);
+
     @Override
     protected void initChannel(Channel channel) throws Exception {
-
+        ChannelPipeline pipeline = channel.pipeline();
+        pipeline.addLast(group, "serverHandler", new ServerHandler());
     }
 }
