9646b6e5daae22557ecdd2b3c03338c98ad84923
Sam1920Chanow <sam.chanow@griffinschool.org>
2017-03-07 13:33:07 -0600

    Fixed most errors in encoder but still some - Sam Chanow

---
+++ src/main/java/in/voidma/classroom/network/core/protocol/EnvelopeDecoder.java
@@ -0,0 +1,8 @@
+package in.voidma.classroom.network.core.protocol;
+
+/**
+ * Created by schan on 3/7/2017.
+ */
+public class EnvelopeDecoder {
+
+}
---
+++ src/main/java/in/voidma/classroom/network/core/protocol/EnvelopeEncoder.java
@@ -0,0 +1,38 @@
+package in.voidma.classroom.network.core.protocol;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.handler.codec.MessageToMessageEncoder;
+
+import in.voidma.classroom.network.core.protocol.Envelope;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.Channel;
+/**
+ * Created by schan on 3/7/2017.
+ */
+public abstract class EnvelopeEncoder extends MessageToMessageEncoder {
+    // ...
+    public static ByteBuf encodeMessage(Envelope message)
+            throws IllegalArgumentException {
+        // verify that no fields are set to null
+
+        // version(1b) + type(1b) + payload length(4b) + payload(nb)
+        int size = 2 + message.getPayload().length;
+
+        ByteBuf buffer = ByteBuf.buffer(size);
+        buffer.writeByte(message.getType().getByteValue());
+        buffer.writeInt(message.getPayload().length);
+        buffer.writeBytes(message.getPayload());
+
+        return buffer;
+    }
+
+    @Override
+    protected abstract void encode(ChannelHandlerContext channelHandlerContext, Channel channel, Object msg) throws Exception {
+        if (msg instanceof Envelope) {
+            return encodeMessage((Envelope)msg);
+        } else {
+            return msg;
+        }
+    }
+}
