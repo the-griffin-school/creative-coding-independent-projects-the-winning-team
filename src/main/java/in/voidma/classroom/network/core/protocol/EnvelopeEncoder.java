95dc91e89d23ed73973733af778de33fceaead2b
Sam1920Chanow <sam.chanow@griffinschool.org>
2017-03-09 11:02:42 -0600

    Think i figured out the bytebuf allocater/had to implement class

--- src/main/java/in/voidma/classroom/network/core/protocol/EnvelopeEncoder.java
+++ src/main/java/in/voidma/classroom/network/core/protocol/EnvelopeEncoder.java
@@ -1,17 +1,18 @@
 package in.voidma.classroom.network.core.protocol;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufAllocator;
 import io.netty.handler.codec.MessageToMessageEncoder;
 
 import in.voidma.classroom.network.core.protocol.Envelope;
+
+import io.netty.buffer.ByteBufAllocator;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.Channel;
 /**
  * Created by schan on 3/7/2017.
  */
-public abstract class EnvelopeEncoder extends MessageToMessageEncoder {
+public abstract class EnvelopeEncoder extends MessageToMessageEncoder implements ByteBufAllocator {
     // ...
     public static ByteBuf encodeMessage(Envelope message)
             throws IllegalArgumentException {
@@ -20,7 +21,7 @@ public abstract class EnvelopeEncoder extends MessageToMessageEncoder {
         // version(1b) + type(1b) + payload length(4b) + payload(nb)
         int size = 2 + message.getPayload().length;
 
-        ByteBuf buffer = ;
+        ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer(size);
         buffer.writeByte(message.getType());
         buffer.writeInt(message.getPayload().length);
         buffer.writeBytes(message.getPayload());
