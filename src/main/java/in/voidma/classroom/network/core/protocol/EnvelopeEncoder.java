078bf144e458a33f474b2a586ffeaaa4e5083bed
Sam1920Chanow <sam.chanow@griffinschool.org>
2017-03-09 10:18:52 -0600

    Ecoder need byteBufAllocater

--- src/main/java/in/voidma/classroom/network/core/protocol/EnvelopeEncoder.java
+++ src/main/java/in/voidma/classroom/network/core/protocol/EnvelopeEncoder.java
@@ -1,6 +1,7 @@
 package in.voidma.classroom.network.core.protocol;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
 import io.netty.handler.codec.MessageToMessageEncoder;
 
 import in.voidma.classroom.network.core.protocol.Envelope;
@@ -19,8 +20,8 @@ public abstract class EnvelopeEncoder extends MessageToMessageEncoder {
         // version(1b) + type(1b) + payload length(4b) + payload(nb)
         int size = 2 + message.getPayload().length;
 
-        ByteBuf buffer = ByteBuf.buffer(size);
-        buffer.writeByte(message.getType().getByteValue());
+        ByteBuf buffer = ;
+        buffer.writeByte(message.getType());
         buffer.writeInt(message.getPayload().length);
         buffer.writeBytes(message.getPayload());
 
