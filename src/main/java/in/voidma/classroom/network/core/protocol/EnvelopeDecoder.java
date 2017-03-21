2acf810c37c078571af0e7b437942f0bbcb2a33a
Sam1920Chanow <sam.chanow@griffinschool.org>
2017-03-21 09:17:55 -0500

    Decoder - create decoder

--- src/main/java/in/voidma/classroom/network/core/protocol/EnvelopeDecoder.java
+++ src/main/java/in/voidma/classroom/network/core/protocol/EnvelopeDecoder.java
@@ -1,8 +1,10 @@
 package in.voidma.classroom.network.core.protocol;
 
+import io.netty.handler.codec.ReplayingDecoder;
+
 /**
  * Created by schan on 3/7/2017.
  */
-public class EnvelopeDecoder {
+public class EnvelopeDecoder extends ReplayingDecoder<DecodingState> {
 
 }
