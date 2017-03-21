1d5dc99bbf427e602cb2a7a67bc62001ad92f690
Sam1920Chanow <sam.chanow@griffinschool.org>
2017-03-21 12:46:46 -0500

    Decoders stuufff

--- src/main/java/in/voidma/classroom/network/core/protocol/EnvelopeEncoder.java
+++ src/main/java/in/voidma/classroom/network/core/protocol/EnvelopeEncoder.java
@@ -3,27 +3,22 @@ package in.voidma.classroom.network.core.protocol;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.MessageToMessageEncoder;
+import io.netty.handler.codec.MessageToByteEncoder;
 
 import java.util.List;
 
 /**
  * Created by schan on 3/7/2017.
  */
-public class EnvelopeEncoder extends MessageToMessageEncoder<Envelope> {
+public class EnvelopeEncoder extends MessageToByteEncoder<Envelope> {
 
     @Override
-    protected void encode(ChannelHandlerContext channelHandlerContext, Envelope message, List out) throws Exception {
+    protected void encode(ChannelHandlerContext channelHandlerContext, Envelope message, ByteBuf out) throws Exception {
         // verify that no fields are set to null
 
         //type(1b) + payload length(4b) + payload(nb)
-        int size = Integer.BYTES + Byte.BYTES + message.getPayload().length;
-
-        ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer(size);
-        buffer.writeByte(message.getType());
-        buffer.writeInt(message.getPayload().length);
-        buffer.writeBytes(message.getPayload());
-
-        out.add(buffer);
+        out.writeByte(message.getType());
+        out.writeInt(message.getPayload().length);
+        out.writeBytes(message.getPayload());
     }
 }
