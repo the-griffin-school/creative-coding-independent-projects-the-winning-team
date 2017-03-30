9d28082accd052d1727132b72277f4894fc2ab64
Silberling-Cook <miles.silberlingcook@gmail.com>
2017-03-30 09:28:20 -0500

    Split client and server files

--- src/main/java/in/voidma/classroom/network/core/gameplay/Player.java
+++ src/main/java/in/voidma/classroom/network/core/gameplay/Player.java
@@ -1,10 +1,9 @@
 package in.voidma.classroom.network.core.gameplay;
 
 import java.util.ArrayList;
-import java.util.UUID;
 
 public class Player extends Entity {
     
     private String name;
-    private ArrayList<Cell> cells;
+    private ArrayList<PCell> cells;
 }
