65ac51f27f1ae1a192f336127e704072872dc291
KingKondak <alvarezzane@gmail.com>
2017-04-04 09:18:05 -0500

    GUI implementation beginning, all

---
+++ src/main/java/in/voidma/classroom/network/client/gui/PlayScreen.java
@@ -0,0 +1,20 @@
+package in.voidma.classroom.network.client.gui;
+
+/**
+ * Created by Zane on 4/4/2017.
+ */
+public class PlayScreen extends Screen {
+
+    public PlayScreen() {
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void draw() {
+
+    }
+}
---
+++ src/main/java/in/voidma/classroom/network/client/gui/Screen.java
@@ -0,0 +1,10 @@
+package in.voidma.classroom.network.client.gui;
+
+/**
+ * Created by Zane on 4/4/2017.
+ */
+public abstract class Screen {
+
+    public abstract void update ();
+    public abstract void draw();
+}
