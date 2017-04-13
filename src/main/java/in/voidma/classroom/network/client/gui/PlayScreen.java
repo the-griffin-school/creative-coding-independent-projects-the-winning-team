3ae437f4eddd04d6d8c7d7ec9b7904aa10507f45
Sam1920Chanow <sam.chanow@griffinschool.org>
2017-04-13 09:28:56 -0500

    added locations and some trnasformation info

--- src/main/java/in/voidma/classroom/network/client/gui/PlayScreen.java
+++ src/main/java/in/voidma/classroom/network/client/gui/PlayScreen.java
@@ -3,7 +3,9 @@ package in.voidma.classroom.network.client.gui;
 import fisica.FWorld;
 import fisica.Fisica;
 import in.voidma.classroom.network.client.Client;
+import in.voidma.classroom.network.client.entity.Cell;
 import in.voidma.classroom.network.client.entity.Player;
+import in.voidma.classroom.network.core.gameplay.entity.ICell;
 
 /**
  * Created by Zane on 4/4/2017.
@@ -28,11 +30,28 @@ public class PlayScreen extends Screen {
 
     @Override
     public void update() {
-
+        world.step(); //TODO: Update with dynamic time increment
     }
 
     @Override
     public void draw() {
+        float averageMass = 0;
+        float averageLocationX = 0;
+        float averageLocationY = 0;
 
+        for (ICell c : ourPlayer.getCells()) {
+            averageMass += c.getMass();
+            averageLocationX += c.getLocation().getX();
+            averageLocationY += c.getLocation().getY();
+        }
+
+        averageMass = averageMass / ourPlayer.getCells().size();
+        averageLocationX = averageLocationX / ourPlayer.getCells().size();
+        averageLocationY = averageLocationY / ourPlayer.getCells().size();
+
+        processing.pushMatrix();
+        
+        world.draw(processing);
+        processing.popMatrix();
     }
 }
