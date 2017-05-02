575c074c983e4d1dbd8a5a256526ce0db60387ab
Sam1920Chanow <sam.chanow@griffinschool.org>
2017-05-02 09:45:42 -0500

    finished ClientBound Packets and serverbound packets

--- src/main/java/in/voidma/classroom/network/core/network/play/Client/CPlayerCellPosition.java
+++ src/main/java/in/voidma/classroom/network/core/network/play/Client/CPlayerCellPosition.java
@@ -5,20 +5,37 @@ import in.voidma.classroom.network.core.network.play.INetHandlerPlayServer;
 import io.netty.buffer.ByteBuf;
 
 import java.io.IOException;
+import java.util.UUID;
 
 /**
  * Created by schan on 4/26/2017.
  */
 public class CPlayerCellPosition implements Packet<INetHandlerPlayServer>{
-    public void readPacketData(ByteBuf buf) throws IOException {
 
+    int x, y;
+    UUID uuid;
+
+    public CPlayerCellPosition(UUID uuid, int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.uuid = uuid;
+    }
+
+    public void readPacketData(ByteBuf buf) throws IOException {
+        uuid = new UUID(buf.readLong(), buf.readLong());
+        x = buf.readInt();
+        y = buf.readInt();
     }
 
     public void writePacketData(ByteBuf buf) throws IOException {
+        buf.writeLong(uuid.getMostSignificantBits());
+        buf.writeLong(uuid.getLeastSignificantBits());
 
+        buf.writeInt(x);
+        buf.writeInt(y);
     }
 
     public void processPacket(INetHandlerPlayServer handler) {
-
+        handler.processPlayerCellPosition(this);
     }
 }
--- src/main/java/in/voidma/classroom/network/core/network/play/Client/CSplit.java
+++ src/main/java/in/voidma/classroom/network/core/network/play/Client/CSplit.java
@@ -5,20 +5,29 @@ import in.voidma.classroom.network.core.network.play.INetHandlerPlayServer;
 import io.netty.buffer.ByteBuf;
 
 import java.io.IOException;
+import java.util.UUID;
 
 /**
  * Created by schan on 4/26/2017.
  */
 public class CSplit implements Packet<INetHandlerPlayServer>{
-    public void readPacketData(ByteBuf buf) throws IOException {
 
+    UUID uuid;
+
+    public CSplit(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public void readPacketData(ByteBuf buf) throws IOException {
+        uuid = new UUID(buf.readLong(), buf.readLong());
     }
 
     public void writePacketData(ByteBuf buf) throws IOException {
-
+        buf.writeLong(uuid.getMostSignificantBits());
+        buf.writeLong(uuid.getLeastSignificantBits());
     }
 
     public void processPacket(INetHandlerPlayServer handler) {
-
+        handler.processSplit(this);
     }
 }
