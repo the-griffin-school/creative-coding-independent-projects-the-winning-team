5d6fec3567ba303563fc68c9b87b948db6709e2c
Silberling-Cook <miles.silberlingcook@gmail.com>
2017-05-16 09:51:19 -0500

    Implemented EntityStore

---
+++ src/main/java/in/voidma/classroom/network/core/entity/EntityStore.java
@@ -0,0 +1,61 @@
+package in.voidma.classroom.network.core.entity;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+
+import java.util.UUID;
+
+/**
+ * Created by Miles on 5/16/2017.
+ */
+public class EntityStore {
+
+    private static EntityStore Instance;
+
+    static {
+        Instance = new EntityStore();
+    }
+
+    private BiMap<UUID, Entity> entityMap;
+
+    private EntityStore() {
+
+        entityMap = HashBiMap.create();
+    }
+
+    public static EntityStore instance() {
+
+        return Instance;
+    }
+
+    public void register(UUID id, Entity entity) {
+
+        entityMap.put(id, entity);
+    }
+
+    public void deregister(Entity entity) {
+
+        entityMap.inverse().remove(entity);
+    }
+
+    public void deregister(UUID id) {
+
+        entityMap.remove(id);
+    }
+
+    public Entity get(UUID id) {
+
+        return entityMap.get(id);
+    }
+
+    public UUID get(Entity entity) {
+
+        return entityMap.inverse().get(entity);
+    }
+
+    public <T extends Entity> T get(UUID id, Class<T> type) {
+
+        Entity entity = entityMap.get(id);
+        return type.cast(entity);
+    }
+}
