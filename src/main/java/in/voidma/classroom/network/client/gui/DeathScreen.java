01ff8faa6e8233d08e99e72ae3dd62a44c52b000
KingKondak <alvarezzane@gmail.com>
2017-05-18 10:03:53 -0500

    removal of some fisica

--- src/main/java/in/voidma/classroom/network/client/gui/DeathScreen.java
+++ src/main/java/in/voidma/classroom/network/client/gui/DeathScreen.java
@@ -1,7 +1,7 @@
 package in.voidma.classroom.network.client.gui;
 
-import fisica.FBody;
-import fisica.FCompound;
+//import fisica.FBody;
+//import fisica.FCompound;
 import in.voidma.classroom.network.client.Client;
 
 import java.util.ArrayList;
@@ -12,14 +12,14 @@ import java.util.ArrayList;
 public class DeathScreen extends Screen {
 
     int finalMass = 0;
-    private FCompound ourPlayer;
+    //private FCompound ourPlayer;
 
     public DeathScreen(Client client) {
         super(client);
 
-        for (FBody body : (ArrayList<FBody>) ourPlayer.getBodies()) {
+       /* for (FBody body : (ArrayList<FBody>) ourPlayer.getBodies()) {
             finalMass += body.getMass();
-        }
+        }*/
     }
 
     public void update(int seconds) {
@@ -29,7 +29,7 @@ public class DeathScreen extends Screen {
     public void draw() {
         processing.background(0);
 
-        processing.text("You have died: " + ourPlayer.getName(), processing.width /2, processing.height/2);
+        processing.text("You have died: ", processing.width /2, processing.height/2);
         processing.text("Final Mass: " + finalMass, processing.width/2, processing.height /2 + (processing.height/ 5));
     }
 }
